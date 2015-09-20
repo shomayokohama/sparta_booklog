7d6319a88c63355d726737a0f3b6e94f4bc6287d
横浜翔馬 <yokohamashoma@yokohama.local>
2015-09-20 11:42:34 +0900

    20/sep/2015バンドルインストールした、サーバーの設定した（mySql）

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :books do
     resources :reviews
   end
-  root 'books#index'
+  root 'restaurants#edit'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
